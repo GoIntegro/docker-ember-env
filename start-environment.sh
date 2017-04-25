3c38b73860cfcbd7b21d9790e5ffbcb484c328a3
Julian Dondero <julian.dondero@gointegro.com>
2017-04-25 10:45:27 -0300

    cambios para levantar automaticamente las aplicaciones

---
+++ start-environment.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+
+if [ -d "/home/user/workspace/go5-web-app/" ]; then    
+    cd /home/user/workspace/go5-web-app/
+    echo "Starting go5-web-app... "
+	ember s --port 4201 &> /home/user/go-social.log &
+else
+	echo "No existe el proyecto go5-web-app en el workspace..."
+fi
+
+if [ -d "/home/user/workspace/go5-account-web/" ]; then
+	cd /home/user/workspace/go5-account-web/
+	echo "Starting go5-account-web... "
+	ember s --port 4202 &> /home/user/go-accounts.log &
+else
+	echo "No existe el proyecto go5-account-web en el workspace..."
+fi
+
+if [ -d "/home/user/workspace/go5-admin-panel/" ]; then
+	cd /home/user/workspace/go5-admin-panel/
+	echo "Starting go5-admin-panel... "
+	ember s --port 4200 &> /home/user/admin-panel.log &
+else
+	echo "No existe el proyecto go5-admin-panel en el workspace..."
+fi
+
+
