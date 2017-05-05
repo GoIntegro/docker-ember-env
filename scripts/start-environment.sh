8cb57ca222556dccd469e4b091d8cb0f5e44d667
Julian Dondero <julian.dondero@gointegro.com>
2017-05-05 14:57:59 -0300

    arreglo config

--- scripts/start-environment.sh
+++ scripts/start-environment.sh
@@ -3,9 +3,10 @@ source /home/user/.nvm/nvm.sh
 
 if [ -d "/home/user/workspace/go5-web-app/" ]; then    
     cd /home/user/workspace/go5-web-app/
-    echo "Starting go5-web-app... "
-    bower install&> /home/user/go-social.log
-    npm install&> /home/user/go-social.log
+    echo "Instalando dependencias gosocial... "
+    bower install
+    npm install
+    echo "*** Starting gosocial ***"
 	ember s --port 4201 --live-reload-port 49154 &> /home/user/go-social.log &
 else
 	echo "No existe el proyecto go5-web-app en el workspace..."
@@ -15,9 +16,10 @@ sleep 3
 
 if [ -d "/home/user/workspace/go5-account-web/" ]; then
 	cd /home/user/workspace/go5-account-web/
-	echo "Starting go5-account-web... "
-	bower install&> /home/user/go-accounts.log
-    npm install&> /home/user/go-accounts.log
+	echo "Instalando dependencias accounts... "	
+	bower install
+    npm install
+    echo "*** Starting accounts ***"
 	ember s --port 4202 --live-reload-port 49155 &> /home/user/go-accounts.log &
 else
 	echo "No existe el proyecto go5-account-web en el workspace..."
@@ -27,9 +29,10 @@ sleep 3
 
 if [ -d "/home/user/workspace/go5-admin-panel/" ]; then
 	cd /home/user/workspace/go5-admin-panel/
-	echo "Starting go5-admin-panel... "
-	bower install&> /home/user/admin-panel.log
-    npm install&> /home/user/admin-panel.log
+	echo "Instalando dependencias admin-panel... "	
+	bower install
+    npm install
+    echo "*** Starting admin-panel ***"
 	ember s --port 4200 --live-reload-port 49153&> /home/user/admin-panel.log &
 else
 	echo "No existe el proyecto go5-admin-panel en el workspace..."
@@ -39,9 +42,10 @@ sleep 3
 
 if [ -d "/home/user/workspace/go5-mobile-app/" ]; then
 	cd /home/user/workspace/go5-mobile-app/
-	echo "Starting go5-mobile-app... "
-	bower install&> /home/user/go-mobile.log
-    npm install&> /home/user/go-mobile.log
+	echo "Instalando dependencias mobile... "	
+	bower install
+    npm install
+    echo "*** Starting mobile ***"
 	ember s --port 4203 --live-reload-port 49156&> /home/user/go-mobile.log &
 else
 	echo "No existe el proyecto go5-mobile-app en el workspace..."
