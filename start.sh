1430845f91b53419b5be020dd105d9bc4f763623
pabloascarza <pabloqac87@gmail.com>
2017-09-07 15:14:12 -0300

    fix rm npm-logk in start script

--- start.sh
+++ start.sh
@@ -4,16 +4,16 @@ app=$1
 
 if [ "$app" == gosocial ]; then
 	echo "Iniciando gosocial... "
-	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-web-app/; source ~/.nvm/nvm.sh; bower install; npm install; ember  s --port 4201 --live-reload-port 49154"
+	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-web-app/; source ~/.nvm/nvm.sh; bower install;rm -f package-lock.json; npm install; ember  s --port 4201 --live-reload-port 49154"
 elif [ "$app" == accounts ]; then
 	echo "Iniciando accounts... "
-	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-account-web/; source ~/.nvm/nvm.sh; bower install; npm install; ember s --port 4202 --live-reload-port 49155"
+	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-account-web/; source ~/.nvm/nvm.sh; bower install;rm -f package-lock.json; npm install; ember s --port 4202 --live-reload-port 49155"
 elif [ "$app" == adminpanel ]; then
 	echo "Iniciando adminpanel... "
-	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-admin-panel/; source ~/.nvm/nvm.sh; bower install; npm install; ember s --port 4200 --live-reload-port 49153"
+	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-admin-panel/; source ~/.nvm/nvm.sh; bower install;rm -f package-lock.json; npm install; ember s --port 4200 --live-reload-port 49153"
 elif [ "$app" == mobile ]; then
 	echo "Iniciando mobile... "
-	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-mobile-app/; source ~/.nvm/nvm.sh; bower install; npm install; ember s --port 4203 --live-reload-port 49156"
+	docker exec -u user -it dockeremberenv_front_1 bash -c "cd /home/user/workspace/go5-mobile-app/; source ~/.nvm/nvm.sh; bower install; rm -f package-lock.json; npm install; ember s --port 4203 --live-reload-port 49156"
 elif [ "$app" == all ]; then
 	echo "Iniciando todas las aplicaciones... "
 	docker exec -u user dockeremberenv_front_1 bash start-environment.sh
